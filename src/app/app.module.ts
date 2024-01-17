2c84b475d67f915004d882e61e3b0eafc1755358
rutvik bhatt <rutikbhatt83477@gmail.com>
2024-01-18 01:18:35 +0530

    Generated required project components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ProductsComponent } from './products/products.component';
+import { InfrastructureComponent } from './infrastructure/infrastructure.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    ProductsComponent,
+    InfrastructureComponent,
+    ContactUsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
