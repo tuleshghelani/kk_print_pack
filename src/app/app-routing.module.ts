5f53263f179739a1ed2d9a7741cfb3ec3035a50b
rutvik bhatt <rutikbhatt83477@gmail.com>
2024-01-19 00:48:41 +0530

    Developed Header & page routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
+import { InfrastructureComponent } from './infrastructure/infrastructure.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // company profile
+  { path: 'products', component: ProductsComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'infrastructure', component: InfrastructureComponent },
+  { path: 'contact-us', component: ContactUsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
